5eadcd10927a296afe08b6128ab87167a1a696e8
susanlovaglio <susan@flatironschool.com>
2016-07-11 22:40:40 -0400

    done

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -12,13 +12,69 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
-    
-    
-    // Implement your functions here
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool{
 
+        let greaterThan75 = ((a + b + c) / 3.0) > 75
+
+        return greaterThan75
+    }
     
+    func passwordCombo(username: String, password: Int) -> String {
+        
+        if password % 3 == 0 {
 
+            switch username {
+                
+            case "Jerry", "Michael", "Elaine":
 
+                return  "Welcome!"
+                
+            default:
+                
+                return  "Access Denied"
+            }
+        }
+        
+        return "Access Denied"
+    }
+    
+    func doIt(a: String, b: Float) -> Float{
+
+        if  Int(a) >= 1 && Int(a) <= 5 && b >= 10.5 && b <= 15.0 {
+            print("becoming true")
+            print(a)
+            print(b)
+            print(Float(a)! + b)
+            
+            return Float(a)! * b
+            
+        }else if b >= 20 && b <= 30.5{
+            print("becoming true")
+            print(a)
+            print(b)
+            print(Float(a)! + b)
+
+            return Float(a)! * b
+        }
+        
+        return 0.0
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- AllTheBasicsTests/VCTests.swift
+++ AllTheBasicsTests/VCTests.swift
@@ -37,21 +37,21 @@ class VCTests: QuickSpec {
 		describe("passwordCombo(_:password:)") {
 			it("Takes a String and an Int.  If the String is equal to Jerry, Elaine or Michael, AND the Int is evenly divisible by 3, then it returns true, otherwise it returns false") {
 				
-                expect(testVC.passwordCombo(username: "Jose", password: 3)).to(equal("Access Denied"))   // bad name
+                expect(testVC.passwordCombo( "Jose", password: 3)).to(equal("Access Denied"))   // bad name
 				
-                expect(testVC.passwordCombo(username: "Elain", password: 3)).to(equal("Access Denied"))   // slight misspelled name
+                expect(testVC.passwordCombo( "Elain", password: 3)).to(equal("Access Denied"))   // slight misspelled name
 
-                expect(testVC.passwordCombo(username: "Jerry", password: 4)).to(equal("Access Denied"))   // bad number
+                expect(testVC.passwordCombo( "Jerry", password: 4)).to(equal("Access Denied"))   // bad number
 
-                expect(testVC.passwordCombo(username: "Jose", password: 4)).to(equal("Access Denied"))   // bad name and bad number
+                expect(testVC.passwordCombo( "Jose", password: 4)).to(equal("Access Denied"))   // bad name and bad number
 
-                expect(testVC.passwordCombo(username: "Jerry", password: 3)).to(equal("Welcome!"))
+                expect(testVC.passwordCombo( "Jerry", password: 3)).to(equal("Welcome!"))
 
-                expect(testVC.passwordCombo(username: "Elaine", password: 6)).to(equal("Welcome!"))
+                expect(testVC.passwordCombo( "Elaine", password: 6)).to(equal("Welcome!"))
 				
-                expect(testVC.passwordCombo(username: "Michael", password: 21)).to(equal("Welcome!"))
+                expect(testVC.passwordCombo( "Michael", password: 21)).to(equal("Welcome!"))
 
-                expect(testVC.passwordCombo(username: "Michael", password: 0)).to(equal("Welcome!"))
+                expect(testVC.passwordCombo( "Michael", password: 0)).to(equal("Welcome!"))
 			}
 		}
 		
@@ -60,17 +60,17 @@ class VCTests: QuickSpec {
 			
 			it("Checks the validity of each paramters and returns their product as a double or, if there's a validation error, it returns 0") {
 				
-				expect(testVC.doIt("3", 12)).to(equal(36.0))
+                expect(testVC.doIt("3", b: 12)).to(equal(36.0))
 				
-				expect(testVC.doIt("3", 12.5)).to(equal(37.5))  // make sure they're keeping precision
+				expect(testVC.doIt("3", b: 12.5)).to(equal(37.5))  // make sure they're keeping precision
 				
-				expect(testVC.doIt("3.5", 12)).to(equal(0.0))   // first arg is not a whole number
+				expect(testVC.doIt("3.5", b: 12)).to(equal(0.0))   // first arg is not a whole number
 				
-				expect(testVC.doIt("7", 12)).to(equal(0.0))   // one arg out of range
+				expect(testVC.doIt("7", b: 12)).to(equal(0.0))   // one arg out of range
 				
-				expect(testVC.doIt("3", 8.1)).to(equal(0.0))  // other arg out of range
+				expect(testVC.doIt("3", b: 8.1)).to(equal(0.0))  // other arg out of range
 				
-				expect(testVC.doIt("6", 8)).to(equal(0.0))    // both args out of range
+				expect(testVC.doIt("6", b: 8)).to(equal(0.0))    // both args out of range
 				
 			}
 		}
