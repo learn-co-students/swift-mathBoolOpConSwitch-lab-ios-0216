e1b387abb00a21dc4cda8df3928c84c095179bd4
ArielSD <ascottdicker@dosomething.org>
2016-08-12 17:38:51 -0400

    Done

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -16,8 +16,44 @@ class ViewController: UIViewController {
     
     
    // Implement your functions here
+    func averageIsAbove75(firstNumber: Double, secondNumber: Double, thirdNumber: Double) -> Bool {
+        let average = (firstNumber + secondNumber + thirdNumber) / 3.0
+        
+        if average > 75 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
+    func passwordCombo(userName: String, password: Int) -> String {
+        if (userName == "Jerry" || userName == "Elaine" || userName == "Michael") && password % 3 == 0 {
+            return "Welcome!"
+        }
+        else {
+            return "Access Denied"
+        }
+    }
     
-
-
+    func numberGenerator(a: String, b: Float) -> Float {
+        if a == "1" && ((b >= 10.5 && b <= 15.0) || (b >= 20.0 && b <= 30.5))  {
+            return 1.0 * b
+        }
+        else if a == "2" && ((b >= 10.5 && b <= 15.0) || (b >= 20.0 && b <= 30.5)) {
+            return 2.0 * b
+        }
+        else if a == "3" && ((b >= 10.5 && b <= 15.0) || (b >= 20.0 && b <= 30.5)) {
+            return 3.0 * b
+        }
+        else if a == "4" && ((b >= 10.5 && b <= 15.0) || (b >= 20.0 && b <= 30.5)) {
+            return 4.0 * b
+        }
+        else if a == "5" && ((b >= 10.5 && b <= 15.0) || (b >= 20.0 && b <= 30.5)) {
+            return 5.0 * b
+        }
+        else {
+            return 0.0
+        }
+    }
 }
